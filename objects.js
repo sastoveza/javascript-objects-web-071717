fd915db1970162fb1154915f96715455620d78f2
Stacey Astoveza <staceyastoveza@transfix.io>
2017-08-28 13:46:43 -0400

    done

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = new Object ({artistName: "songTitle"})
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName]=songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  var key = artistName
+  delete playlist[key]
+  return playlist
+}
